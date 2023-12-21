5f9fac4f37ca375d3f3d62c6f5ffbd45bf2bf330
Denis <DenisArtisanDeveloppeur@gmail.com>
2023-12-21 16:29:08 +0100

    SearchBar ProductTable

--- installation/src/App.jsx
+++ installation/src/App.jsx
@@ -1,28 +1,66 @@
+import { Input } from "./components/forms/Input.jsx";
+import { Checkbox } from "./components/forms/Checkbox.jsx";
+import { ProductCategoryRow } from "./components/products/ProductCategoryRow.jsx";
+import { ProductRow } from "./components/products/ProductRow.jsx";
 import { useState } from "react"
 
+const PRODUCTS = [
+  { category: "Fruits", price: "$1", stocked: true, name: "Apple" },
+  { category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit" },
+  { category: "Fruits", price: "$2", stocked: false, name: "Passionfruit" },
+  { category: "Vegetables", price: "$2", stocked: true, name: "Spinach" },
+  { category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin" },
+  { category: "Vegetables", price: "$1", stocked: true, name: "Peas" }
+]
 
 function App() {
 
-  const [isTermAccepted, setIsTermAccepted] = useState(false)
 
   return <>
-    <h1>React.js</h1>
-    <form >
-      <CGUCheckbox checked={isTermAccepted} onCheck={setIsTermAccepted} />
-      <button disabled={!isTermAccepted}>Envoyer le formulaire</button>
-    </form>
+    <h1>React.js : Liste de produit</h1>
+    <div className="container my-3">
+      <SearchBar />
+      <ProductTable products={PRODUCTS} />
+    </div>
   </>
 }
 
-function CGUCheckbox({ checked, onCheck }) {
+function SearchBar() {
   return <div>
-    <label>
-      <input type="checkbox"
-        onChange={(e) => onCheck(e.target.checked)}
-        checked={checked} />
-      Accepter les conditions d'utilisations
-    </label>
+    <div className="mb-3">
+      <Input value="" onChange={() => null} placeholder="Rechercher..." />
+      <Checkbox id="stocked" checked={false} onChange={() => null} label="N'afficher que les produits en stock" />
+    </div>
   </div>
 }
 
+function ProductTable({ products }) {
+
+  const rows = []
+  let lastCategory = null
+
+  for (let product of products) {
+
+    if (product.category !== lastCategory) {
+      rows.push(<ProductCategoryRow key={product.category} name={product.category} />)
+    }
+    lastCategory = product.category
+    rows.push(
+      <ProductRow product={product} key={product.name} />
+    )
+
+  }
+
+  return <table className="table">
+    <thead>
+      <tr>
+        <th>Nom</th>
+        <th>Prix</th>
+      </tr>
+    </thead>
+    <tbody>
+      {rows}
+    </tbody>
+  </table>
+}
 export default App
