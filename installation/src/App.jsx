00a537cb25c844e47346dd6724d19e4611e1c319
Denis <DenisArtisanDeveloppeur@gmail.com>
2023-12-21 12:54:06 +0100

    hook useState

--- installation/src/App.jsx
+++ installation/src/App.jsx
@@ -1,35 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react"
 
+/*const todos = [
+  'Presenter react',
+  'Présenter le JSX',
+  'Créer des composants'
+]
+*/
 function App() {
+
+  const [person, setPerson] = useState({
+    firstName: 'John', lastName: 'Doe', age: 18
+
+  })
+
+  const incrementAge = () => {
+    setPerson({ ...person, age: person.age + 1 })
+  }
   const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  const incrementCount = () => {
+    console.log('render')
+    setCount(count + 1)
+    //setCount((c) => c + 1)
+
+  }
+
+  return <>
+    <h1>React.js</h1>
+    <p>Age {person.firstName} : {person.age}</p>
+    <button onClick={incrementAge}>Gagner une année</button>
+    <p>Compteur : {count}</p>
+    <button onClick={incrementCount}> Incrémenter {count}</button>
+
+
+    {/*
+    <ul>
+      {todos.map(todo => (<li key={todo}>{todo}</li>))}
+    </ul>
+    */}
+  </>
+
 }
 
 export default App
