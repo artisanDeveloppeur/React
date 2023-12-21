7b7792b8e34af9ce684ec44ed6fa2ea13ad2b5a0
Denis <DenisArtisanDeveloppeur@gmail.com>
2023-12-21 13:26:48 +0100

    formulaires

--- installation/src/App.jsx
+++ installation/src/App.jsx
@@ -1,43 +1,64 @@
 import { useState } from "react"
 
-/*const todos = [
-  'Presenter react',
-  'Présenter le JSX',
-  'Créer des composants'
-]
-*/
+
 function App() {
+  //    <h2>Approche controlée </h2>
+  /*
+    const [firstName, setFirstName] = useState('John Doe')
+  
+    const handleChange = (e) => {
+      setFirstName(e.target.value)
+    }
+  
+    const reset = () => { setFirstName('') }
+  */
 
-  const [person, setPerson] = useState({
-    firstName: 'John', lastName: 'Doe', age: 18
+  //<h2>Approche non controlée </h2>
+  /*
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(new FormData(e.target))
+  */
 
-  })
-
-  const incrementAge = () => {
-    setPerson({ ...person, age: person.age + 1 })
+  const [value, setValue] = useState('')
+  const handleChange = (e) => {
+    setValue(e.target.value)
   }
-  const [count, setCount] = useState(0)
 
-  const incrementCount = () => {
-    console.log('render')
-    setCount(count + 1)
-    //setCount((c) => c + 1)
-
+  const [checked, setChecked] = useState(true)
+  const toggleCheck = () => {
+    setChecked(!checked)
   }
 
   return <>
     <h1>React.js</h1>
-    <p>Age {person.firstName} : {person.age}</p>
-    <button onClick={incrementAge}>Gagner une année</button>
-    <p>Compteur : {count}</p>
-    <button onClick={incrementCount}> Incrémenter {count}</button>
-
-
-    {/*
-    <ul>
-      {todos.map(todo => (<li key={todo}>{todo}</li>))}
-    </ul>
+    {/* 
+    <h2>Approche controlée </h2>
+    <form>
+      <label>FirstName</label>
+      <input type="text" name="firstName"
+        value={firstName} onChange={handleChange} />
+      {firstName}
+      <button onClick={reset} type="button">Reset</button>
+    </form>
     */}
+
+    {/* <h2>Approche non controlée</h2> */}
+    {/* 
+    <form onSubmit={handleSubmit}>
+      <label>FirstName</label>
+      <input type="text" name="firstName" defaultValue="default" />
+      <button>Envoyer</button>
+    </form>
+    */}
+    <form >
+      <label>FirstName</label>
+      {/*<input type="text" name="firstName" value={value} onChange={handleChange} />*/}
+      <textarea value={value} onChange={handleChange} />
+      <input type="checkbox" checked={checked} onChange={toggleCheck} />
+      {/*{checked && <button>Envoyer</button>} */}
+      <button disabled={!checked}>Envoyer</button>
+    </form>
   </>
 
 }
