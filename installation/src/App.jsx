cc1de77e451b35b8ff76d491ed1ba84422cb4496
Denis <DenisArtisanDeveloppeur@gmail.com>
2023-12-22 11:03:11 +0100

    Title

--- installation/src/App.jsx
+++ installation/src/App.jsx
@@ -5,51 +5,16 @@ import { useEffect, useState } from "react"
 
 function App() {
 
-  const [showInput, setShowInput] = useState(true)
 
 
   return <>
-    <h1>React.js : hook useEffect</h1>
+    <h1>React.js : useMemo</h1>
     <div className="container my-3 stack">
-      <Checkbox
-        id="titleshow"
-        checked={showInput}
-        onChange={setShowInput}
-        label="Afficher le champ titre"
-      />
 
-      {showInput && <EditTitle />}
     </div>
   </>
 }
 
-function EditTitle() {
-  const [title, setTitle] = useState('')
-  const [firtsName, setFirstname] = useState('')
 
-  useEffect(() => {
-    const originalTitle = document.title
-    return () => {
-      document.title = originalTitle
-    }
-  }, [])
 
-  useEffect(() => {
-    console.log(title)
-    document.title = title
-  }, [title]);
-
-  return <div className="vstack gap-2">
-    <Input
-      placeholder="Modifier le titre"
-      value={title}
-      onChange={setTitle}
-    />
-    <Input
-      placeholder="Prénom"
-      value={firtsName}
-      onChange={setFirstname}
-    />
-  </div>
-}
 export default App
