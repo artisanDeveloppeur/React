507e0960341570ef916f32ad4033158cfb68e06d
Denis <DenisArtisanDeveloppeur@gmail.com>
2023-12-23 08:30:35 +0100

    useMemo useId

--- installation/src/components/forms/Input.jsx
+++ installation/src/components/forms/Input.jsx
@@ -1,11 +1,17 @@
+import { useId } from "react"
+
 /**
  * @param {string} placeholder
+ * @param {string} label
  * @param {string} value
  * @param {(s: string) => void} onChange
  */
-export function Input({ placeholder, value, onChange }) {
+export function Input({ label, placeholder, value, onChange }) {
+    const id = useId()
     return <div>
+        <label className="form-label" htmlFor={id}>{label}</label>
         <input
+            id={id}
             type="text"
             className="form-control"
             value={value}
