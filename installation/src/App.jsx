368e1815f24b6e74191fc3b1eb2fb384dabbc58d
Denis <DenisArtisanDeveloppeur@gmail.com>
2023-12-24 10:50:54 +0100

    forwardRef useRef

--- installation/src/App.jsx
+++ installation/src/App.jsx
@@ -24,7 +24,6 @@ function App() {
     <h1>React.js : useRef</h1>
     <div className="container my-3 stack gap-2">
       <Input label={"prefix"} value={prefix} onChange={setPrefix} />
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam scelerisque mi nec lectus aliquet elementum. Pellentesque convallis sollicitudin neque non tempor. Quisque vel ipsum finibus, facilisis arcu nec, tincidunt ipsum. Donec faucibus congue porttitor.
     </div>
   </>
 }
--- installation/src/components/forms/Input.jsx
+++ installation/src/components/forms/Input.jsx
@@ -1,4 +1,4 @@
-import { useId } from "react"
+import { forwardRef, useId } from "react"
 
 /**
  * @param {string} placeholder
@@ -6,6 +6,31 @@ import { useId } from "react"
  * @param {string} value
  * @param {(s: string) => void} onChange
  */
+export const Input = forwardRef(
+    function Input({ label, placeholder, value, onChange }
+        , ref) {
+        const id = useId()
+        return <div>
+            <label className="form-label" htmlFor={id}>{label}</label>
+            <input
+                ref={ref}
+                id={id}
+                type="text"
+                className="form-control"
+                value={value}
+                placeholder={placeholder}
+                onChange={(e) => onChange(e.target.value)}
+            />
+        </div>
+    })
+
+Input.displayName = 'InputComponent'
+{/*
+onChange={(e) => onChange?.(e.target.value)}  
+<Input label={"prefix"} value={prefix} />
+*/}
+
+{/*
 export function Input({ label, placeholder, value, onChange }) {
     const id = useId()
     return <div>
@@ -20,3 +45,4 @@ export function Input({ label, placeholder, value, onChange }) {
         />
     </div>
 }
+*/}
