6392bab839663ceab259a81b236460c9825ac7de
Denis <DenisArtisanDeveloppeur@gmail.com>
2023-12-24 10:36:17 +0100

    timer useRef

--- installation/src/App.jsx
+++ installation/src/App.jsx
@@ -1,16 +1,30 @@
-//import { Input } from "./components/forms/Input.jsx";
+import { Input } from "./components/forms/Input.jsx";
 //import { Checkbox } from "./components/forms/Checkbox.jsx";
-import { useState } from "react"
+import { useEffect, useRef, useState } from "react"
 
 
 function App() {
 
 
+  const prefixRef = useRef(null) // useMemo(() => ({ current: null }), [])
+  const [prefix, setPrefix] = useState('')
+  prefixRef.current = prefix
+
+  useEffect(() => {
+
+    const timer = setInterval(() => {
+      console.log(prefixRef.current)
+    }, 1000)
+    return () => {
+      clearInterval(timer)
+    }
+  }, []);
 
   return <>
     <h1>React.js : useRef</h1>
     <div className="container my-3 stack gap-2">
-
+      <Input label={"prefix"} value={prefix} onChange={setPrefix} />
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam scelerisque mi nec lectus aliquet elementum. Pellentesque convallis sollicitudin neque non tempor. Quisque vel ipsum finibus, facilisis arcu nec, tincidunt ipsum. Donec faucibus congue porttitor.
     </div>
   </>
 }
