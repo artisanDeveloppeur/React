4e4597160d0cd5066056c8aa8a0d6fbfd03daf6e
Denis <DenisArtisanDeveloppeur@gmail.com>
2024-01-01 13:59:17 +0100

    setInterval clearInterval timer

--- installation/src/App.jsx
+++ installation/src/App.jsx
@@ -1,30 +1,15 @@
-import { Input } from "./components/forms/Input.jsx";
+//import { Input } from "./components/forms/Input.jsx";
 //import { Checkbox } from "./components/forms/Checkbox.jsx";
-import { useEffect, useRef, useState } from "react"
+//import { useState } from "react"
 
 
 function App() {
 
 
-  const prefixRef = useRef(null) // useMemo(() => ({ current: null }), [])
-  const [prefix, setPrefix] = useState('')
-  prefixRef.current = prefix
-
-  useEffect(() => {
-
-    const timer = setInterval(() => {
-      console.log(prefixRef.current)
-    }, 1000)
-    return () => {
-      clearInterval(timer)
-    }
-  }, []);
 
   return <>
-    <h1>React.js : useRef</h1>
-    <div className="container my-3 stack gap-2">
-      <Input label={"prefix"} value={prefix} onChange={setPrefix} />
-    </div>
+    <h1>React.js : hook personnalisé</h1>
+
   </>
 }
 
