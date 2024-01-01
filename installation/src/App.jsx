996bd6e82a0526b47d35e2d1b574786a955fad9d
Denis <DenisArtisanDeveloppeur@gmail.com>
2024-01-01 15:32:56 +0100

    useFetch

--- installation/src/App.jsx
+++ installation/src/App.jsx
@@ -1,4 +1,4 @@
-import { useFetch } from "./hooks/useFetch"
+import { useFetch } from "./hooks/useFetch.js"
 
 
 
@@ -8,17 +8,25 @@ import { useFetch } from "./hooks/useFetch"
 
 function App() {
 
-  // https://jsonplaceholder.typicode.com/posts?_limit=10&_delay=2000
   const { loading, data, errors } = useFetch('https://jsonplaceholder.typicode.com/posts?_limit=10&_delay=2000')
 
 
   return <>
     <h1>React.js : hook personnalisé</h1>
-    {loading && <div>Chargement ...</div>}
-    {data && <div>{JSON.stringify(data)}</div>}
+    {loading && <div className="spinner-border" role="status">
+      <span className="visually-hidden">Loading...</span>
+    </div>}
+    {errors && <div className="alert alert-danger">{errors.toString()}</div>}
+    {data && <div>
+      <ul>
+        {data.map(post => (<li key={post.id}>{post.title}</li>))}
+      </ul>
+    </div>}
   </>
 }
 
 
 
 export default App
+
+//  {data && <div>{JSON.stringify(data)}</div>}
